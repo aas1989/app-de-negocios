41a4b5b51812394bae59a39a35a3e0268b24b28c
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-15 10:20:47 -0300

    first commit

---
+++ src/BottleSong.java
@@ -0,0 +1,12 @@
+public class BottleSong{
+    public static void main(String[] args) {
+        int bottlesNum = 10;
+        String word = "bottles";
+
+        while (bottlesNum > 0) {
+            if (bottlesNum == 1) {
+                word = "bottle";
+            }
+        }
+    }
+}
