d777576348101a48e22e63d3bc25c64cb95a8caf
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-15 11:01:43 -0300

    atualizado

--- src/BottleSong.java
+++ src/BottleSong.java
@@ -1,12 +1,22 @@
 public class BottleSong{
     public static void main(String[] args) {
         int bottlesNum = 10;
-        String word = "bottles";
+        String word = "garrafas";
 
         while (bottlesNum > 0) {
             if (bottlesNum == 1) {
-                word = "bottle";
+                word = " garrafa ";
             }
+            System.out.println( bottlesNum +" "+ word + " verde , pendurada na parede");
+            System.out.println( bottlesNum  +" "+ word + " verde , pendurada na parede");
+            System.out.println(" E se uma garrafa derde cair acidentalmente, ");
+                bottlesNum = bottlesNum - 1;
+            
+                if (bottlesNum > 0) {
+                    System.out.println("Restará " + bottlesNum +" "+ word + " verde , pendurada na parede");
+                } else {
+                    System.out.println(" Restará " + bottlesNum + " não haverá garrafas verdes, pendurada na parede.");
+                }
         }
     }
 }
